7913cf7ad365ced2db934fc83ea4dd81a1a001b2
Felipe Santos <ffss@cin.ufpe.br>
2023-04-11 12:04:47 -0300

    Update stopwatch.v

--- src/stopwatch.v
+++ src/stopwatch.v
@@ -1,4 +1,4 @@
-module stopwatch(input clk, output reg[31:0] seconds);
+module stopwatch(input clk, input[2:0] tag1, output reg[31:0] seconds);
 	
 	initial begin
 		seconds = 0;
@@ -9,17 +9,21 @@ module stopwatch(input clk, output reg[31:0] seconds);
 	reg[3:0] m_sec_cnt = 0;	
 	
 	always @ (posedge clk) begin
-		if(sec_cnt < clk_machine / 10) begin
-			sec_cnt <= sec_cnt + 1;
+		if(tag1 == 1) begin
+			if(sec_cnt < clk_machine / 10) begin
+				sec_cnt <= sec_cnt + 1;
+			end
+			else begin 
+				sec_cnt <= 0;
+				seconds <= seconds + 1;
+			end
+			
+			if(seconds > 9999) begin
+				seconds <= 0;
+			end
 		end
-		else begin 
-			sec_cnt <= 0;
-			seconds <= seconds + 1;
-		end
-		
-		if(seconds > 9999) begin
+		else if(tag1 == 2) begin
 			seconds <= 0;
 		end
-		
 	end
 endmodule 
