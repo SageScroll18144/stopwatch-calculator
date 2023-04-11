f5facf78211726dbdfd1d1963f3307cee3c65214
Felipe Santos <ffss@cin.ufpe.br>
2023-04-11 12:04:20 -0300

    Update main.v

--- src/main.v
+++ src/main.v
@@ -1,5 +1,6 @@
 module main(
 	input clk,
+	inout reg[2:0] tag1,
 	output [31:0] seconds, 
 	output [0:6] display_centenas,
 	output [0:6] display_dezenas,
@@ -11,19 +12,41 @@ module main(
 	output [3:0] keyword
 );
 	
-	//stopwatch(clk, seconds);
-	keypad(clk, LINE, COLLUMMN, keyword);
-	
 	reg[3:0] centena;
 	reg[3:0] dezena;
 	reg[3:0] unidade;	
 	reg[3:0] dec_seconds;
 	
-	always @ (*) begin
-		centena <= keyword / 1000;
-		dezena <= (keyword % 1000) / 100;
-		unidade <= (keyword % 100) / 10;
-		dec_seconds <= keyword % 10;
+	reg [31:0] real_seconds;
+	
+	initial begin
+		tag1 <= 1;
+		real_seconds <= 0;
+	end
+	
+	stopwatch(clk, tag1, seconds);
+	keypad(clk, LINE, COLLUMMN, keyword);
+	
+	always @ (posedge clk) begin
+		if(keyword == 11) begin
+			tag1 <= 1;
+			real_seconds <= seconds; 
+		end
+		else if(keyword == 10) begin
+			real_seconds <= 0;
+			tag1 <= 2;
+		end
+		else if(keyword == 12) begin
+			tag1 <= 1;
+		end
+		else if(keyword == 13) begin
+			tag1 <= 0;
+		end
+	
+		centena <= real_seconds / 1000;
+		dezena <= (real_seconds % 1000) / 100;
+		unidade <= (real_seconds % 100) / 10;
+		dec_seconds <= real_seconds % 10;
 	end
 	
 	display(centena, display_centenas);
