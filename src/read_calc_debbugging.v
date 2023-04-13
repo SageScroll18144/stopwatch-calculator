c56fe6dcdb3b373cf527e34cbc577a32cd89385e
Mend25 <113634496+Mend25@users.noreply.github.com>
2023-04-13 09:08:49 -0300

    Update read_calc_debbugging.v

--- src/read_calc_debbugging.v
+++ src/read_calc_debbugging.v
@@ -4,26 +4,22 @@ module read_cal(
 	input [1:0] pressed,
 	output reg [31:0] In1
 );
-	reg released;
 	reg flag_enter1;
 	reg flag_enter2;
 	
 	initial begin
 		flag_enter1 = 0;
 		flag_enter2 = 0;
-		released = 0;
 		In1 = 0;
 	end
 		
 	always @ (posedge pressed) begin
-		if(pressed && ~released && ~flag_enter1) begin
+		if(~flag_enter1) begin
 			In1 <= 10 * number;
 			flag_enter1 <= 1;
 		end
 		
-		if(~pressed && flag_enter1) released <= 1;
-		
-		if(pressed && released && ~flag_enter2) begin
+		else if(~flag_enter2 && flag_enter1) begin
 			In1 <= In1 + number;
 			flag_enter2 <= 1;
 		end
