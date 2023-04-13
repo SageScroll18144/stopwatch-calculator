b6e13ccb0051b8bb1b81a18f4028ce08d7a250b5
Ana Paula Sá <114103289+anapsa@users.noreply.github.com>
2023-04-13 11:33:31 -0300

    Update read_calc_debugging.v
    Implementação do delay

--- src/read_calc_debugging.v
+++ src/read_calc_debugging.v
@@ -10,6 +10,8 @@ module read_cal_debugging(
 	reg flag_enter1;
 	reg flag_enter2;
 	reg flagIn2;
+	reg[31:0] cnt = 0;
+	parameter delay_time = 2;
 	
 	initial begin
 		flag_enter1 = 0;
@@ -21,34 +23,41 @@ module read_cal_debugging(
 	end
 		
 	always @ (posedge pressed) begin
-		if(~flag_enter1 && number < 10) begin
-			if(~flagIn2) In1 <= 10 * number;
-			else In2 <= 10 * number;
-			flag_enter1 <= 1;
+		if(cnt < delay_time) begin
+			cnt <= cnt + 1;
 		end
+		else begin 
+			cnt <= 0;
 		
-		else if(~flag_enter2 && flag_enter1 && number < 10) begin
-			if(~flagIn2) In1 <= In1 + number;
-			else In2 <= In2 + number;
-			flag_enter2 <= 1;
-		end
-		
-		else if(number == 15) begin
-			flag_enter1 <= 0;
-			flag_enter2 <= 0;
-			led1 <= 1;
-			led2 <= 0;
-			flagIn2 <= 0;
-			In1 <= 0;
-		end
-		
-		else if(number == 14) begin
-			flag_enter1 <= 0;
-			flag_enter2 <= 0;
-			led1 <= 0;
-			led2 <= 1;
-			flagIn2 <= 1;
-			In2 <= 0;
+			if(~flag_enter1 && number < 10) begin
+				if(~flagIn2) In1 <= 10 * number;
+				else In2 <= 10 * number;
+				flag_enter1 <= 1;
+			end
+			
+			else if(~flag_enter2 && flag_enter1 && number < 10) begin
+				if(~flagIn2) In1 <= In1 + number;
+				else In2 <= In2 + number;
+				flag_enter2 <= 1;
+			end
+			
+			else if(number == 15) begin
+				flag_enter1 <= 0;
+				flag_enter2 <= 0;
+				led1 <= 1;
+				led2 <= 0;
+				flagIn2 <= 0;
+				In1 <= 0;
+			end
+			
+			else if(number == 14) begin
+				flag_enter1 <= 0;
+				flag_enter2 <= 0;
+				led1 <= 0;
+				led2 <= 1;
+				flagIn2 <= 1;
+				In2 <= 0;
+			end
 		end
 	end
 		
