72270b46ec360d977d4f04f36e6d7ffa105f98ac
Mend25 <113634496+Mend25@users.noreply.github.com>
2023-04-12 23:24:42 -0300

    Create read_calc_debbugging.v

---
+++ src/read_calc_debbugging.v
@@ -0,0 +1,35 @@
+module read_cal(
+	input clk,
+	input[31:0] number,
+	output reg [31:0] In1, 
+	input [3:0] Col
+);
+	reg released;
+	reg flag_enter1;
+	reg flag_enter2;
+	
+	initial begin
+		flag_enter1 = 0;
+		flag_enter2 = 0;
+		released = 0;
+		In1 = 0;
+	end
+		
+	always @ (posedge clk) begin
+		if(~&Col) begin
+			if(~released && ~flag_enter1) begin
+				In1 <= 10 * number;
+				flag_enter1 <= 1;
+			end
+		end
+		
+		if(&Col && flag_enter1) released <= 1;
+		
+		if(~&Col && released && ~flag_enter2) begin
+			In1 <= In1 + number;
+			flag_enter2 <= 1;
+		end
+		
+	end
+		
+endmodule 
