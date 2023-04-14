9facd0870620fbdb90bfbedd5ccbb2cefca24bc6
Mend25 <113634496+Mend25@users.noreply.github.com>
2023-04-13 21:53:08 -0300

    Update calculator_debugging.v

--- src/calculator_debugging.v
+++ src/calculator_debugging.v
@@ -4,12 +4,14 @@ module calculator(
 	input [3:0] keyboard, 
 	input modo,
 	output reg [31:0] answer, 
-	output reg signal
+	output reg signal,
+	output reg flag_answer
 ); 
 	
 	initial begin 
 		answer = 0; 
 		signal = 0;
+		flag_answer = 0;
 	end 
 	
 	parameter A = 4'd10, B = 4'd11, C = 4'd12, D = 4'd13;
@@ -20,6 +22,7 @@ module calculator(
 			if(keyboard == A) begin 
 				answer <= In1 + In2; 
 				signal <= 0; 
+				flag_answer <= 1;
 			end 
 			else if(keyboard == B) begin 
 				if(In2 > In1) begin
@@ -31,10 +34,13 @@ module calculator(
 					answer <= In1 - In2;
 					signal <= 0;
 				end 
+				
+				flag_answer <= 1;
 			end 
 			else if(keyboard == C) begin 
 				answer <=  In1 * In2; 
-				signal <= 0; 
+				signal <= 0;
+				flag_answer <= 1;	
 			end 
 			if(keyboard == D) begin
 				signal <= 0; 
